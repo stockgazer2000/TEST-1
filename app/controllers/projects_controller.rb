a46339c0bdd6db9f0ec966413b18bf5385783f89
Neophyte <stockgazer_2000@yahoo.com>
2011-12-05 18:58:29 -0600

    added update, updt blank chk and delete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,10 +16,10 @@ class ProjectsController < ApplicationController
   #end
   
   def create
-  @project = Project.new(params[:project])
+   @project = Project.new(params[:project])
    if @project.save
        flash[:notice] = "Project has been created."
-        redirect_to @project
+       redirect_to @project
     else
        flash[:alert] = "Project has not been created."
        render :action => "new"
@@ -31,5 +31,26 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   
   end
+  
+  def edit
+     @project = Project.find(params[:id])
+  end
 
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+       flash[:notice] = "Project has been updated."
+       redirect_to @project
+    else
+       flash[:alert] = "Project has not been updated."
+       render :action => "edit"
+    end  
+  end
+  
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+end
 end

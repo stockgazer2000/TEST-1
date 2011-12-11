be873e9ad49605658ebd6680b67531f97a078bd2
Neophyte <stockgazer_2000@yahoo.com>
2011-12-11 02:00:43 -0600

    added feature for signing in via the devise provided form

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,6 +1,8 @@
 # To change this template, choose Tools | Templates
 Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
+    unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes)
+    @user.confirm! unless unconfirmed
   end
 end
